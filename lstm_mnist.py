6ae5a613eab674f5feb130411173b98bb287fc85
praveendareddy21 <praveendareddy@gmail.com>
2017-03-20 06:29:53 -0700

    updated Utililty to be more configurable

--- lstm_mnist.py
+++ lstm_mnist.py
@@ -12,19 +12,22 @@ tf.app.flags.DEFINE_integer('batch_size', 10, 'use batch normalization. otherwis
 FLAGS = tf.flags.FLAGS
 
 
-class Utility:
-  #holds FLAGS and other variables that are used in different files
-  def __init__(self):
-    global FLAGS
-    self.FLAGS = FLAGS
-    self.tf_data_type = {}
-    self.tf_data_type["double"] = tf.float64
-    self.tf_data_type["float"] = tf.float32
-    self.np_data_type = {}
-    self.np_data_type["double"] = np.float64
-    self.np_data_type["float"] = np.float32
+class Config:
+	def __init__(self):
+		global FLAGS
+		self.FLAGS = FLAGS
+		self.tf_data_type = {}
+		self.tf_data_type["double"] = tf.float64
+		self.tf_data_type["float"] = tf.float32
+		self.np_data_type = {}
+		self.np_data_type["double"] = np.float64
+		self.np_data_type["float"] = np.float32
+		self.learning_rate = 0.001
+		self.decay = 0.9
 
-    #self.random = Random(FLAGS.python_seed)
+	def print_config(self):
+		print("learning_rate" +" : "+ str(self.learning_rate))
+		print("decay" +" : "+ str(self.decay))
 
 utility = Utility()
 
