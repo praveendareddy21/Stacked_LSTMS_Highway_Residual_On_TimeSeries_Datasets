93caa2ea62efbc0558088cee89dd0fec7094fe26
praveendareddy21 <praveendareddy@gmail.com>
2017-03-27 20:07:10 -0700

    fixed erraneous git commits caused by pycharm

---
+++ mnist_data_handler.py
@@ -0,0 +1,7 @@
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
+trX = trX.reshape(-1, 28, 28)
+teX = teX.reshape(-1, 28, 28)
