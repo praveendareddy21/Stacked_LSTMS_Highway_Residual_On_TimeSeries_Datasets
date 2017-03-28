62eebbd4f4a7f08ad1c6b522a76b98868a20d73e
praveendareddy21 <praveendareddy@gmail.com>
2017-03-28 11:32:31 -0700

    added config class for HAR dataset and refined mnist data handler

--- main_runner.py
+++ main_runner.py
@@ -17,7 +17,7 @@ FLAGS = tf.flags.FLAGS
 
 
 
-class Config:
+class Config(object):
 	def __init__(self):
 		global FLAGS
 		self.FLAGS = FLAGS
@@ -37,15 +37,55 @@ class Config:
     #self.random = Random(FLAGS.python_seed)
 
 
+class HARConfig(Config):
+	def __init__(self):
+		super(HARConfig, self).__init__()
+		self.train_count = 7352  # 7352 training series
+		self.test_data_count = 2947  # 2947 testing series
+		self.n_steps = 128 # 128 time_steps per series
+		self.n_classes = 6  # Final output classes
 
+		# Training
+		self.learning_rate = 0.001
+		self.lambda_loss_amount = 0.005
+		self.training_epochs = 250
+		self.batch_size = 100
+		self.clip_gradients = 15.0
+		self.gradient_noise_scale = None
+		# Dropout is added on inputs and after each stacked layers (but not
+		# between residual layers).
+		self.keep_prob_for_dropout = 0.85  # **(1/3.0)
+
+		# Linear+relu structure
+		self.bias_mean = 0.3
+		# I would recommend between 0.1 and 1.0 or to change and use a xavier
+		# initializer
+		self.weights_stddev = 0.2
+
+		########
+		# NOTE: I think that if any of the below parameters are changed,
+		# the best is to readjust every parameters in the "Training" section
+		# above to properly compare the architectures only once optimised.
+		########
+
+		# LSTM structure
+		# Features count is of 9: three 3D sensors features over time
+		#self.n_inputs = len(X_train[0][0])
+		self.n_hidden = 12  # 28  # nb of neurons inside the neural network
+		# Use bidir in every LSTM cell, or not:
+		self.use_bidirectionnal_cells = False
+
+		# High-level deep architecture
+		self.also_add_dropout_between_stacked_cells = False  # True
 
 
 for learning_rate in [0.0001, 0.002]: #1, 0.0025, 0.002]:  # [0.01, 0.007, 0.001, 0.0007, 0.0001]:
     for decay in [0.9]: #[0.005, 0.01]:
-    	config = Config()
-    	config.learning_rate = learning_rate
-    	config.decay = decay
-       	run_with_config(config) #, trX, trY, teX, teY)
+		config = HARConfig()
+		#config = Config()
+		config.learning_rate = learning_rate
+		config.decay = decay
+		run_with_config(config) #, trX, trY, teX, teY)
 
 
 
--- mnist_data_handler.py
+++ mnist_data_handler.py
@@ -1,7 +1,10 @@
 from tensorflow.examples.tutorials.mnist import input_data
 
 
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
-trX = trX.reshape(-1, 28, 28)
-teX = teX.reshape(-1, 28, 28)
+
+def get_mnist_data():
+	mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+	trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
+	trX = trX.reshape(-1, 28, 28)
+	teX = teX.reshape(-1, 28, 28)
+	return (trX, trY, teX, teY)
