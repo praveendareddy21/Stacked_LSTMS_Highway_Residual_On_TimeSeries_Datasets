bbd238dff0152c2ac7d2bfd131da6e29e24618e2
praveendareddy21 <praveendareddy@gmail.com>
2017-04-27 11:50:58 -0700

    updated model import in main runner for better config capability

--- main_runner.py
+++ main_runner.py
@@ -4,7 +4,14 @@ import numpy as np
 from random import Random
 from tensorflow.contrib import rnn
 from tensorflow.python.framework import ops
-from lstm_mnist import run_with_config
+
+
+import lstm_mnist
+import bi_dir_residual_lstm_HAR
+import deep_lstm_model
+
+
+
 
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('data_dir', '',
@@ -70,13 +77,20 @@ class HARConfig(Config):
 
 		# LSTM structure
 		# Features count is of 9: three 3D sensors features over time
-		#self.n_inputs = len(X_train[0][0])
+		self.n_inputs = 0
 		self.n_hidden = 12  # 28  # nb of neurons inside the neural network
 		# Use bidir in every LSTM cell, or not:
 		self.use_bidirectionnal_cells = False
 
 		# High-level deep architecture
 		self.also_add_dropout_between_stacked_cells = False  # True
+		self.n_layers_in_highway = 1
+		self.n_stacked_layers = 3
+
+
+
+run_with_config = deep_lstm_model.run_with_config
+
 
 
 for learning_rate in [0.0001, 0.002]: #1, 0.0025, 0.002]:  # [0.01, 0.007, 0.001, 0.0007, 0.0001]:
