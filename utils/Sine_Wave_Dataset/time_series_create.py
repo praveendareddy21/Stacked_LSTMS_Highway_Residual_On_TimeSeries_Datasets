ce6556e7526b673e4b5a82a80947aaaef3b26a67
praveendareddy21 <praveendareddy@gmail.com>
2017-05-01 15:19:43 -0700

    got it working

--- utils/Sine_Wave_Dataset/time_series_create.py
+++ utils/Sine_Wave_Dataset/time_series_create.py
@@ -3,7 +3,7 @@
 ##
 import sys
 import numpy as np
-import SlidingWindowGenerator
+from SlidingWindowGenerator import SlidingWindowGenerator
 
 
 def loadfile(infile):
@@ -12,13 +12,58 @@ def loadfile(infile):
         data = inf.readlines()
     return data
 
+def loadfileWithStrippedNewline(infile):
+    data = []
+    with open(infile, 'r') as inf:
+        data = inf.readlines()
+
+    stripped_data = []
+    for i in data:
+    	stripped_data.append(i.rstrip())
+    return stripped_data
+
 def savefile(outfile, dataset, ids):
     with open(outfile,'w') as of:
         for id in ids:
             of.write(dataset[id])
 
+def saveListsToCsvfile(outfile, dataset, ids):
+    with open(outfile,'w') as of:
+        for id in ids:
+        	for i, value in enumerate(dataset[id]):
+        		if i == len(dataset[id]) - 1:
+        			of.write(value+"\n")
+        		else:
+        			of.write(value+",")
+
+
 def perform_sliding_winow(infile, ratio):
-	pass
+    dataset = loadfileWithStrippedNewline(infile)
+    dataset = SlidingWindowGenerator(dataset, window_size=5).runSlidingWindow(max_dataset_size = 0)
+    print
+    total = len(dataset)
+    if len(dataset) <= 0:
+        print('load empty file, quit')
+        sys.exit(-1)
+
+    if ratio < 0:
+        print('ratio should be in (0,1), reset it to 0.2')
+        ratio = 0.2
+        testcnt = int(total*ratio)
+    elif ratio > 1:
+        print('set absolute test number as %d'%ratio)
+        testcnt = int(ratio)
+    else:
+        testcnt = int(total*ratio)
+    
+
+    id = np.arange(total)
+    perm = np.random.permutation(id)
+    
+    test = perm[:testcnt]
+    train = perm[testcnt:]
+    saveListsToCsvfile('train-' + infile, dataset, train)
+    saveListsToCsvfile('test-' + infile, dataset, test)
 
 def process_split(infile, ratio):
 
@@ -91,4 +136,4 @@ if __name__ == '__main__':
         infile2 = sys.argv[3]
         process_split_X_Y_to_train_test(infile, infile2, ratio)
     else:
-        process_split(infile, ratio)
+        perform_sliding_winow(infile, ratio)
