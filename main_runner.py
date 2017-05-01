dade31a15758179076d33dd1a56d4bb6191d0b73
praveendareddy21 <praveendareddy@gmail.com>
2017-05-01 11:10:51 -0700

    added highway_lstm _model to project

--- main_runner.py
+++ main_runner.py
@@ -14,13 +14,15 @@ import deep_lstm_model
 import single_layer_lstm
 import deep_lstm_model_with_BN
 import deep_lstm_model_on_ucr_dataset
+import highway_lstm_model
+
 
 
 
 if __name__ == '__main__':
 
-	run_with_config = single_layer_lstm.run_with_config
-	config = single_layer_lstm.config
+	run_with_config = highway_lstm_model.run_with_config
+	config = highway_lstm_model.config
 
 	#run_with_config = deep_lstm_model.run_with_config
 	#config = deep_lstm_model.config
