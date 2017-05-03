9d3ba55173e2aabb64c5029664a869feebd3e469
praveendareddy21 <praveendareddy@gmail.com>
2017-05-02 23:02:17 -0700

    added tensorboard to single layer lstm and some refinement

--- single_layer_lstm.py
+++ single_layer_lstm.py
@@ -65,7 +65,7 @@ def LSTM_Network(feature_mat, config):
 X_train, y_train, X_test, y_test = get_HAR_data()
 
 
-class SingleLayerConfig(object):
+class SingleLayerConfig(Config):
     """
     define a class to store parameters,
     the input should be feature mat of training and testing
@@ -81,7 +81,7 @@ class SingleLayerConfig(object):
         # Trainging
         self.learning_rate = 0.0025
         self.lambda_loss_amount = 0.0015
-        self.training_epochs = 300
+        self.training_epochs = 3
         self.batch_size = 1500
 
         # LSTM structure
@@ -89,7 +89,10 @@ class SingleLayerConfig(object):
         self.n_hidden = 32  # nb of neurons inside the neural network
         self.n_classes = 6  # Final output classes
 
-        self.logs_path = '/tmp/various_LSTMS/single_layer/'
+        self.tensor_board_logging_enabled = True
+        self.logs_path = "/tmp/LSTM_logs/single_layer_lstm"
+        self.tensorboard_cmd = "tensorboard --logdir="+ self.logs_path
+
 
 
 #config = Config(X_train, X_test)
@@ -98,6 +101,8 @@ config = SingleLayerConfig()
 
 def run_with_config(config) : #, X_train, y_train, X_test, y_test):
     tf.reset_default_graph()  # To enable to run multiple things in a loop
+    config.print_config()
+
 
     config.W = {
         'hidden': tf.Variable(tf.random_normal([config.n_inputs, config.n_hidden])),
@@ -125,7 +130,6 @@ def run_with_config(config) : #, X_train, y_train, X_test, y_test):
     X = tf.placeholder(tf.float32, [None, config.n_steps, config.n_inputs])
     Y = tf.placeholder(tf.float32, [None, config.n_classes])
 
-    # pred_Y = LSTM_Network(X, config)
     pred_Y = LSTM_Network(X, config)
 
     print "Unregularised variables:"
@@ -144,6 +148,14 @@ def run_with_config(config) : #, X_train, y_train, X_test, y_test):
 
     correct_pred = tf.equal(tf.argmax(pred_Y, 1), tf.argmax(Y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_pred, dtype=tf.float32))
+    # ------------------------------------------------------
+    # step3.5 : Tensorboard stuff here
+    # ------------------------------------------------------
+    if config.tensor_board_logging_enabled:
+        tf.summary.scalar("loss", cost)
+        tf.summary.scalar("accuracy", accuracy)
+        merged_summary_op = tf.summary.merge_all()
+
 
     # --------------------------------------------
     # step4: Hooray, now train the neural network
@@ -152,13 +164,24 @@ def run_with_config(config) : #, X_train, y_train, X_test, y_test):
     sess = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=False))
     tf.global_variables_initializer().run()
 
+    if config.tensor_board_logging_enabled:
+        # op to write logs to Tensorboard
+        summary_writer = tf.summary.FileWriter(config.logs_path, graph=tf.get_default_graph())
+
     best_accuracy = 0.0
     # Start training for each batch and loop epochs
     for i in range(config.training_epochs):
         for start, end in zip(range(0, config.train_count, config.batch_size),
                               range(config.batch_size, config.train_count + 1, config.batch_size)):
-            sess.run(optimizer, feed_dict={X: X_train[start:end],
-                                           Y: y_train[start:end]})
+            if config.tensor_board_logging_enabled :
+                _, summary = sess.run([optimizer, merged_summary_op], feed_dict={X: X_train[start:end],Y: y_train[start:end]})
+            else:
+                sess.run(optimizer, feed_dict={X: X_train[start:end],Y: y_train[start:end]})
+
+
+        if config.tensor_board_logging_enabled:
+            # Write logs at every iteration
+            summary_writer.add_summary(summary, i)
 
         # Test completely at every epoch: calculate accuracy
         pred_out, accuracy_out, loss_out = sess.run([pred_Y, accuracy, cost], feed_dict={
@@ -172,4 +195,13 @@ def run_with_config(config) : #, X_train, y_train, X_test, y_test):
     print("final test accuracy: {}".format(accuracy_out))
     print("best epoch's test accuracy: {}".format(best_accuracy))
     print("")
+
+    if config.tensor_board_logging_enabled:
+        print("Run the command line:\n")
+        print(config.tensorboard_cmd)
+        print("\nThen open http://0.0.0.0:6006/ into your web browser")
+
+if __name__ == '__main__':
+    run_with_config(config) #, trX, trY, teX, teY)
+
 
