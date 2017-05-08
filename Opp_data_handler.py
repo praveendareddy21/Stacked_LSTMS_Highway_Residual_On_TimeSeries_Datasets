b0bd792fe7a3d6130ed140560d2bdc4e94ecdfb5
praveendareddy21 <praveendareddy@gmail.com>
2017-05-08 15:24:16 -0700

    added bare-bone shell for Opportunity data handler

---
+++ Opp_data_handler.py
@@ -0,0 +1,53 @@
+import np
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def get_Opp_data_with_time_series_size():
+	return (X_train, y_train, X_test, y_test, time_series_size)
+if __name__ == '__main__':
+  print(get_Opp_data_with_time_series_size())
+ 
