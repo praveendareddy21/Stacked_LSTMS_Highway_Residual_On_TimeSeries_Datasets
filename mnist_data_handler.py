d5dba1d65a5eff061d773b746c1befa0d353d376
praveendareddy21 <praveendareddy@gmail.com>
2017-05-08 15:16:47 -0700

    updated mnist to match UCR's data-handlers

--- mnist_data_handler.py
+++ mnist_data_handler.py
@@ -8,3 +8,10 @@ def get_mnist_data():
 	trX = trX.reshape(-1, 28, 28)
 	teX = teX.reshape(-1, 28, 28)
 	return (trX, trY, teX, teY)
+
+def get_mnist_data_with_time_series_size():
+	mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+	trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
+	trX = trX.reshape(-1, 28, 28)
+	teX = teX.reshape(-1, 28, 28)
+	return (trX, trY, teX, teY, 28)
