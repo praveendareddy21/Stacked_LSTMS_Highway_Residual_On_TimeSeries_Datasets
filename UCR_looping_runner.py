7a3b6cfcfee428b91eb7e12bbcec285a46759494
praveendareddy21 <praveendareddy@gmail.com>
2017-05-15 06:43:46 -0700

    checkpoint before linking model with dataset

--- UCR_looping_runner.py
+++ UCR_looping_runner.py
@@ -10,12 +10,7 @@ import lstm_mnist
 
 
 
-import deep_lstm_model
-import single_layer_lstm
-import highway_lstm_model
-import residual_lstm_model
-import  deep_lstm_model_UCR_dataset
-import highway_lstm_model_UCR_dataset
+import single_layer_lstm_looper_UCR
 
 def get_immediate_subdirectories(a_dir):
     return [name for name in os.listdir(a_dir)
@@ -30,5 +25,30 @@ def run_with_dataset(dataset):
 def modify_config_wtih_current_dataset(config):
     pass
 
+
+run_with_config = single_layer_lstm_looper_UCR.run_with_config
+config = single_layer_lstm_looper_UCR.config
+
+def looper_runner(UCR_dataset_name):
+    run_with_config(config)
+
+
 if __name__ == '__main__':
     print (get_dataset_list())
+    exit(0)
+
+    run_with_config = single_layer_lstm_looper_UCR.run_with_config
+    config = single_layer_lstm_looper_UCR.config
+
+    for learning_rate in [0.005, 0.0025, 0.003, 0.0005]:  # 1, 0.0025, 0.002]:  # [0.01, 0.007, 0.001, 0.0007, 0.0001]:
+        for decay in [0.9]:  # [0.005, 0.01]:
+            for bn_enabled in [True, False]:
+                for n_stacked in [2, 3, 4, 6]:  # 2 3 6
+                    for epoch_count in [200, 300, 350]:
+                        config.training_epochs = epoch_count
+                        config.tensor_board_logging_enabled = False  # should be always False, log summary folder gets impacted by mulitple runs
+                        config.n_stacked_layers = n_stacked
+                        config.batch_norm_enabled = bn_enabled
+                        config.learning_rate = learning_rate
+                        config.decay = decay
+                        run_with_config(config)  # , trX, trY, teX, teY)
